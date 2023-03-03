1cb6fdac58c3ee22c0368f2d546eca04ead045aa
abdoachhoubi <aachhoub@gmail.com>
2023-03-03 19:42:32 +0100

    Added Level01 Challenge

---
+++ Level01/index.js
@@ -0,0 +1,6 @@
+function addLength(str) {
+  //start-here
+}
+
+// Don't change this line -_-
+export default addLength;
---
+++ Tests/test01.test.js
@@ -0,0 +1,55 @@
+import addLength from "../Level01/index.js";
+import COLORS from "./utils.js";
+
+const test_a = () =>
+{
+    const result = addLength("apple ban");
+    if (result[0] === "apple 5" && result[1] === "ban 3") {
+        console.log(COLORS.GREEN, "Test A passed");
+    }
+    else
+    {
+        console.log(COLORS.RED, "Test A failed");
+    }
+    console.log("Your result", result);
+    console.log("Expected result", ["apple 5", "ban 3"]);
+}
+
+const test_b = () =>
+{
+    const result = addLength("you will win");
+    if (result[0] === "you 3" && result[1] === "will 4" && result[2] === "win 3") {
+        console.log(COLORS.GREEN, "Test B passed");
+    }
+    else
+    {
+        console.log(COLORS.RED, "Test B failed");
+    }
+    console.log("Your result", result);
+    console.log("Expected result", ["you 3", "will 4", "win 3"]);
+}
+
+const test_c = () =>
+{
+    const result = addLength("you");
+    if (result[0] === "you 3") {
+        console.log(COLORS.GREEN, "Test C passed");
+    }
+    else
+    {
+        console.log(COLORS.RED, "Test C failed");
+    }
+    console.log("Your result", result);
+    console.log("Expected result", ["you 3"]);
+}
+
+try
+{
+    test_a();
+    test_b();
+    test_c();
+}
+    catch (e) {
+    console.error(COLORS.RED, "Test failed ");
+    console.error(COLORS.RED, e.message);
+}
